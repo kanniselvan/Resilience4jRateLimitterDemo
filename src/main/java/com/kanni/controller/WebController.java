2aff43a08cccc8b196f087cad472f590ef0d6454
Kanniselvan <selvankanni@gmail.com>
2022-11-09 21:49:31 +0530

    Rate limit changes

---
+++ src/main/java/com/kanni/controller/WebController.java
@@ -0,0 +1,30 @@
+package com.kanni.controller;
+
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping("/home")
+public class WebController {
+
+    @RateLimiter(name="processService", fallbackMethod = "processFallback")
+    @GetMapping
+    public String  welcomeString(){
+      /*  try {
+            Thread.sleep(TimeUnit.SECONDS.toSeconds(2));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        return UUID.randomUUID().toString()+"   "+ LocalDateTime.now();
+    }
+
+    public String processFallback(Throwable throwable){
+        return  LocalDateTime.now()+ "=== "+throwable.getMessage();
+    }
+}
---
+++ src/main/java/com/kanni/test/RateLimiterTestCase.java
@@ -0,0 +1,22 @@
+package com.kanni.test;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+public class RateLimiterTestCase {
+
+    public static void main(String arg[]){
+
+
+       RestTemplate restTemplate=new RestTemplate();
+
+        for(int i=1;i<100;i++) {
+            String output = restTemplate.getForObject("http://localhost:8080/home", String.class);
+            log.info(output);
+        }
+
+    }
+}
